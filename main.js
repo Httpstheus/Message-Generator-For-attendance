87d846e4726c413b93403c3bde1bea910336bf33
Matheus Rodrigues <84551135+Httpstheus@users.noreply.github.com>
2022-12-08 15:26:49 -0300

    Update main.js
    Add Mask for CPF and Telephone

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+
+  document.getElementById('telefone').addEventListener('input', function (e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+  });
+
+const input = document.getElementById('cpf')
+
+      input.addEventListener('keypress', () => {
+          let inputLength = input.value.length
+
+          // MAX LENGHT 14  CPF
+          if (inputLength == 3 || inputLength == 7) {
+              input.value += '.'
+          }else if (inputLength == 11) {
+              input.value += '-'
+          }
+      },
+
+
  
   $(document).ready(function(){
     $('#btn-send').on('click', validarDados);
@@ -5,27 +25,29 @@
     function validarDados() {
       let tipo           = $('#tipo').val();
       let nome           = $('#nome').val();
+     
       let valor          = $('#valor').val(); 
       let protocolo      = $('#protocolo').val();
-      let telefone      = $('#telefone').val();
+      let telefone       = $('#telefone').val();
       let juros          = $('#juros').val ();
       let parcela        = ($('#parcela').val() == '') ? 1: $('#parcela').val();
-      let valorDesc      = (valor - (valor *0.1)).toFixed(2); 
+      let valorDesc      = (valor - (valor *0.3)).toFixed(2); 
       let valorParcela   = (valorDesc/parcela).toFixed(2); ;
       let mensagem       = '';
-      
-
-        var valueEmprise = document.getElementById("emprise").value;
-        document.getElementById("resposta").innerHTML = valueEmprise;
-
-        
-    
-
+      let cpfCliente     = $('#cpf').val();
+      let replaceNumbers = '.XXX.XXX';
+      let inicioReplace = cpfCliente.substring(0,3);
+      let endReplace = cpfCliente.substring(11);
+      let resultMaskReplace = inicioReplace + replaceNumbers + endReplace;
 
       
+      var valueEmprise = document.getElementById("emprise").value;
+            document.getElementById("resposta").innerHTML = valueEmprise;
+  
+   
       switch(tipo) {
 
-          // Message for Option == Mundial Editora
+          // Message for Option 1
           case 'carta01':
             mensagem =
             // Nr.Document & telephone client
@@ -43,7 +65,7 @@
             messageFinished = document.getElementById("resposta").innerHTML = mensagem;
 
           break;
-        // Message for Option == Edit Brasil
+        // Message for Option 2
           case 'carta02':
             mensagem =
                     // Nr.Document & telephone client
@@ -59,7 +81,7 @@
                            'horas será dado continuidade as medidas cabíveis.  ' + '<br><br>' +
                            'Valor da sua documentação: R$ ' + juros +
                            
-                           '<br><br> DIGITE 1: Quitação por PIX ou Boleto a vista por R$ ' +valorDesc +
+                           '<br><br> *DIGITE 1*: Quitação por PIX ou Boleto a vista por R$ ' +valorDesc +
                            '<br>*DIGITE 2*Quitação pelo cartão de crédito ' +parcela + ' vezes de R$ ' +valorParcela +
                            '<br><br> *Qual melhor opção para pagamento HOJE?*';
                            
@@ -67,11 +89,80 @@
             messageFinished = document.getElementById("resposta").innerHTML = mensagem;
 
           break;
+
+                  // Message for Option 3
+                  case 'carta03':
+                    mensagem =
+                            // Nr.Document & telephone client
+                               'Nr: ' +protocolo + ' || Telefone: ' +telefone + '<br> <br>' +
+             
+                            //    Message 
+                                   '*PROCESSO DE EXECUÇÃO PARA AÇÃO INTERNA* <br><br>' +
+                                   
+                                   'Olá ' + nome + '  <br>' +
+                                   'Ação Interna aberta em seu nome e CPF, com pedido de visita do *Oficial de Justiça* para avaliação de *Penhora de Bens*. <br><br>' +
+                                   'Processo: ' +protocolo + '<br>' +
+                                   'CPF: ' + resultMaskReplace + '<br>'+
+                                   'Valor da Ação: '  +juros + 
+
+                                   '<br> Proposta de conciliação: R$'  +valorDesc +
+                                   '<br><br>Para Conciliação Online responda *EU QUERO*'   
+        
+                    messageFinished = document.getElementById("resposta").innerHTML = mensagem;
+        
+                  break;
+
+                                    // Message for Option 4
+                  case 'carta04':
+                    mensagem =
+                            // Nr.Document & telephone client
+                               'Nr: ' +protocolo + ' || Telefone: ' +telefone + '<br> <br>' +
+             
+                            //    Message 
+                                   '*PROCESSO EXTRAJUDICIAL* <br><br>' +
+                                   
+                                   'Sr.(a) ' + nome + '  <br>' +
+                                   'Ação Interna no valor de *' + juros +'* <br><br>' +
+                                   '*Atualmente se encontra com uma ação interna, EM ANDAMENTO, protocolada em nosso escritório*. <br>' +
+                                   'Você pode consultar em:  https://advocaciamaximos.com/#consultar <br>'+
+                                   'Está para ser agendada a *VISITA DO OFICIAL DE JUSTIÇA COM PEDIDO PRÉVIO DE INTIMAÇÃO*<BR><BR>'+
+                                   'Esta libera a conciliação amigável sem custo Advocatício, para abrir *DIGITE SIM*.'                 
+        
+                    messageFinished = document.getElementById("resposta").innerHTML = mensagem;
+        
+                  break;
+        
+                                    // Message for Option 5
+                  case 'carta05':
+                    mensagem =
+                            // Nr.Document & telephone client
+                               'Nr: ' +protocolo + ' || Telefone: ' +telefone + '<br> <br>' +
+             
+                            //    Message 
+                                   '*NOTIFICAÇÃO DE PEDIDO DE CONDENAÇÃO DE PAGAMENTO* <br><br>' +
+                                   
+                                   'Nome ' + nome + '  <br>' +
+                                   'Protocolo: ' +protocolo + '<br><br>' +
+
+                                   '*Ação Interna executada* aguardando posicionamento do requerido. <br>' +
+                                   'Para consulta do seu Processo: https://advocaciamaximos.com/resultado#consultar. <br><br>' +
+                                   '*Valor em Processo: R$' + juros + '<br>' +
+                                   '*DIGITE 1* - Boleto quitação à vista *R$' + valorDesc + '*<br>' +
+                                   '*DIGITE 2* - Cartão de crédito em *12 vezes sem juros* (Podendo ser de Terceiros) <br>' +
+                                   '*DIGITE 3* - Nova proposta de pagamento. <br><br>' +
+                                   'Digite uma opção e receba os valores.'
+
+                    messageFinished = document.getElementById("resposta").innerHTML = mensagem;
+        
+                  break;
+        
        
       }
+
+      
     } 
       
-  });
+  }));
 
   function CopyToClipboard(resposta){
     var r = document.createRange();
